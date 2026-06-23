f10823fc0890847a72a0ed60a0ba14e159216735
sinhaayushi-oss <sinhaayushi1310@gmail.com>
2026-06-23 22:30:02 +0530

    Add files via upload

---
+++ implementation/BeautifulMatrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0, col = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                int x = sc.nextInt();
+
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int moves = Math.abs(row - 3) + Math.abs(col - 3);
+
+        System.out.println(moves);
+
+        sc.close();
+    }
+}
